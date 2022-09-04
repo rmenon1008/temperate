9953a7cec15c48b6b531a74df2bb9863e7fd03a5
rmenon1008 <rmenon1008@gmail.com>
2022-09-03 20:51:15 -0700

    Adds image swapping Switches to css animation vs transition to handle load

--- src/components/Background/Background.js
+++ src/components/Background/Background.js
@@ -24,55 +24,74 @@ const Background = (props) => {
     // }, [props.imgUrl]);
     const [profileName, setProfileName] = useState(undefined);
     const [profileLink, setProfileLink] = useState(undefined);
+    const [loaded, setLoaded] = useState(false);
     // const [opacity, setOpacity] = useState(0);
-    // const [justSwapped, setJustSwapped] = useState(false);
+
+    const updateImage = () => {
+        let imageQuery = API_BASE + userStorage.background.imgSource;
+        if (userStorage.background.imgOffset && userStorage.background.imgOffset !== 0) {
+            imageQuery += "_" + (userStorage.background.imgOffset + 1);
+        }
+        fetch(imageQuery).then(response => response.json()).then(data => {
+            setProfileName(data.credit);
+            setProfileLink(data.profile);
+            if (data.url != userStorage.background.imgUrl) {
+                resetImageAnimation();
+                setLoaded(false);
+                setUserStorage((prev) => {
+                    return {
+                        ...prev,
+                        background: {
+                            ...prev.background,
+                            imgUrl: data.url,
+                            imgOffset: userStorage.background.justSwapped ? prev.background.imgOffset : 0,
+                            justSwapped: false,
+                        }
+                    }
+                });
+            }
+            if (userStorage.colors.autoColor) {
+                setUserStorage((prev) => {
+                    return {
+                        ...prev,
+                        colors: {
+                            ...prev.colors,
+                            cbg: "#ffffff",
+                            c1: data.primaryColor,
+                            c2: data.color,
+                            c3: data.bgColor,
+                        }
+                    }
+                });
+            }
+        });
+    }
+
+    const resetImageAnimation = () => {
+        const el = backgroundImgRef.current;
+        el.style.animation = 'none';
+        console.log(el.offsetHeight); /* trigger reflow */
+        el.style.animation = null;
+    }
+
+    useEffect(() => {
+        setTimeout(() => {
+            setLoaded(true);
+        }, 1000);
+    }, [userStorage.background.imgUrl]);
 
     useEffect(() => {
         if (userStorage.background.autoImage) {
-            let imageQuery = API_BASE + userStorage.background.imgSource;
-            if (userStorage.background.imgOffset && userStorage.background.imgOffset !== 0) {
-                imageQuery += "_" + (userStorage.background.imgOffset + 1);
-            }
-            fetch(imageQuery).then(response => response.json()).then(data => {
-                setProfileName(data.credit);
-                setProfileLink(data.profile);
-                if (data.url != userStorage.background.imgUrl) {
-                    backgroundImgRef.current.style.opacity = '0';
-                    setUserStorage((prev) => {
-                        // setJustSwapped(false);
-                        return {
-                            ...prev,
-                            background: {
-                                ...prev.background,
-                                imgUrl: data.url,
-                                imgOffset: prev.background.imgUrl === 'swapped' ? prev.background.imgOffset : 0,
-                            }
-                        }
-                    });
-                }
-                if (userStorage.colors.autoColor) {
-                    setUserStorage((prev) => {
-                        return {
-                            ...prev,
-                            colors: {
-                                ...prev.colors,
-                                cbg: "#ffffff",
-                                c1: data.primaryColor,
-                                c2: data.color,
-                                c3: data.bgColor,
-                            }
-                        }
-                    });
-                }
-            });
+            updateImage();
         }
     }, [userStorage.background.imgSource, userStorage.background.imgOffset, userStorage.colors.autoColor, userStorage.colors.autoImage]);
 
     const makeVisible = () => {
-        setTimeout(() => {
-            backgroundImgRef.current.style.transform = "scale(1)";
-            backgroundImgRef.current.style.opacity = 1;
-        }, 10);
+        // setTimeout(() => {
+        //     backgroundImgRef.current.style.transform = "scale(1)";
+        //     backgroundImgRef.current.style.opacity = 1;
+        // }, 10);
+        setLoaded(true);
     }
 
     const swapImage = (e) => {
@@ -82,7 +101,7 @@ const Background = (props) => {
                 background: {
                     ...prev.background,
                     imgOffset: (prev.background.imgOffset + 1) % 3,
-                    imgUrl: "swapped",
+                    justSwapped: true,
                 }
             }
         });
@@ -90,7 +109,7 @@ const Background = (props) => {
 
     return (
         <div className="Background">
-            <img ref={backgroundImgRef} alt="background" onLoad={makeVisible} src={userStorage.background.imgUrl} draggable="false" />
+            <img className={!loaded ? "unloaded" : null} style={{ opacity: loaded ? 1 : 0 }} ref={backgroundImgRef} alt="background" onLoad={makeVisible} src={userStorage.background.imgUrl} draggable="false" />
             <Attribution name={profileName} link={profileLink} show={!!profileName} swapImage={swapImage} />
         </div>
     );
