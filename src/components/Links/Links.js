52a78ebf232d0093c5839200bb1aaa5ec4eb2c30
rmenon1008 <rmenon1008@gmail.com>
2022-09-03 20:52:09 -0700

    Added support for nested link structure

--- src/components/Links/Links.js
+++ src/components/Links/Links.js
@@ -1,20 +1,41 @@
 import './Links.css'
 import React, { useContext } from 'react';
 import { UserStorageContext } from '../UserStorage/UserStorage';
+import { ChevronDown } from '@styled-icons/heroicons-outline/ChevronDown';
 
 const Links = (props) => {
     const context = useContext(UserStorageContext);
     const userStorage = context.userStorage;
+    
 
     return (
         <div className="Links">
             {userStorage.links.map((link, index) => {
-                if (link.link !== "" && link.text !== "") {
-                    return (
-                        <a href={link.link} className="link" rel="noopener noreferrer">
-                            {link.text}
-                        </a>
-                    );
+                if (link.text !== "") {
+                    if (link.nested) {
+                        return (
+                            <div className="nested-group">
+                                <span className="link category" target="_blank" rel="noopener noreferrer">{link.text}&nbsp;&nbsp;<ChevronDown /></span>
+                                <div className="nested-links-contain">
+                                    <div className="nested-links">
+                                        {link.nested.map((linkNested, indexNested) => {
+                                            if (linkNested.text !== "") {
+                                                return (
+                                                    <a className="nested-link" href={linkNested.link} target="_blank" rel="noopener noreferrer">{linkNested.text}</a>
+                                                )
+                                            }
+                                        })}
+                                    </div>
+                                </div>
+                            </div>
+                        )
+                    } else {
+                        return (
+                            <a href={link.link} className="link" rel="noopener noreferrer">
+                                {link.text}
+                            </a>
+                        );
+                    }
                 } else {
                     return null;
                 }
--- src/components/options/Options/Options.js
+++ src/components/options/Options/Options.js
@@ -1,40 +1,75 @@
 import './Options.css';
 import { Formik, Form, Field, ErrorMessage, FieldArray } from 'formik';
 import { Autosave, SwitchField, ColorField, TextField, SliderField, BackgroundPicker } from '../Form/Form';
-import Logo from '../../Logo/Logo';
-import React, { useContext } from 'react';
+import { Logo } from '../../Logo/Logo';
+import React, { useContext, useEffect } from 'react';
 import { UserStorageContext } from '../../UserStorage/UserStorage';
-import { MinusCircle, PlusCircle } from '@styled-icons/heroicons-outline';
+import { MinusCircle, PlusCircle, FolderAdd } from '@styled-icons/heroicons-outline';
 
 const Options = (props) => {
     document.title = "Temperate Options";
     const userStorage = useContext(UserStorageContext).userStorage;
     const setUserStorage = useContext(UserStorageContext).setUserStorage;
-    console.log("LINKS", userStorage.links);
+
+    useEffect(() => {
+        let link = document.querySelector("link[rel~='icon']");
+        if (!link) {
+            link = document.createElement('link');
+            link.rel = 'icon';
+            document.getElementsByTagName('head')[0].appendChild(link);
+        }
+        link.href = 'favicon-green.svg';
+    }, []);
+
     return (
         <div className="Options">
             <div className="centered-narrow">
                 <Logo />
-                <h1>Preferences</h1>
+                <h1>Options</h1>
                 <Formik
                     initialValues={userStorage}
+                    enableReinitialize={true}
                     onSubmit={(values, { setSubmitting }) => {
                         setUserStorage((prev) => { return { ...prev, ...values } });
                         setSubmitting(false);
                     }}>
-                    {({ isSubmitting }) => (
+                    {({ isSubmitting, setFieldValue, setValues }) => (
                         <Form>
                             <Autosave />
-
+                            <div className="card" id="global">
+                                <h2>General</h2>
+                                <div className="form-group">
+                                    <SwitchField
+                                        name="global.usePreciseLocation"
+                                        label="Use your precise location for weather data"
+                                        onSwitch={() => setFieldValue("global.locationOverride", "")}
+                                    />
+                                    <TextField
+                                        name="global.locationOverride"
+                                        label="Location override"
+                                        disabled={userStorage.global.usePreciseLocation}
+                                        detail="Override IP-based location estimation"
+                                        placeholder="latitude, longitude"
+                                        spellCheck={false}
+                                    />
+                                    <SwitchField name="global.use24h" label="Use 24 hour time" />
+                                    <SliderField
+                                        name="global.uiScale"
+                                        label="User interface size adjust"
+                                        min={0.2}
+                                        max={4}
+                                        step={0.1}
+                                        float
+                                    />
+                                </div>
+                            </div>
                             <div className="card" id="background">
                                 <h2>Background</h2>
                                 <div className="form-group">
                                     <SwitchField
                                         name="background.autoImage"
                                         label="Background images that update daily"
-                                        onSwitchOff={() => {
-                                            setUserStorage((prev) => { return { ...prev, background: { ...prev.background, imgUrl: "" } } });
-                                        }}
+                                        onSwitch={(turnedOn) => setFieldValue("background.imgUrl", "")}
                                     />
                                     <BackgroundPicker
                                         name="background.imgSource"
@@ -44,7 +79,8 @@ const Options = (props) => {
                                     />
                                     <TextField
                                         name="background.imgUrl"
-                                        label="Background image URL"
+                                        label="Background image override"
+                                        placeholder="https://example.com/image.jpg"
                                         spellCheck={false}
                                         disabled={userStorage.background.autoImage}
                                     />
@@ -54,15 +90,32 @@ const Options = (props) => {
                                 <h2>Temperature</h2>
                                 <div className="form-group">
                                     <SwitchField name="temperature.display" label="Show the current temperature" />
-                                    <SwitchField name="temperature.displayCond" label="Show the current condition" />
-                                    <SwitchField name="temperature.useCelsius" label="Use Celsius instead of Fahrenheit" />
-                                    <SwitchField name="temperature.useFeelsLike" label="Use the Feels Like temperature" />
+                                    <SwitchField name="temperature.displayCond" label="Show the current condition" disabled={!userStorage.temperature.display} />
+                                    <SwitchField name="temperature.useCelsius" label="Use Celsius instead of Fahrenheit" disabled={!userStorage.temperature.display} />
+                                    <SwitchField name="temperature.useFeelsLike" label="Use the Feels Like temperature" disabled={!userStorage.temperature.display} />
                                     <SliderField
+                                        disabled={!userStorage.temperature.display}
                                         name="temperature.tempScale"
                                         label="Temperature size adjust"
                                         min={0.2}
                                         max={4}
                                         step={0.1}
+                                        float
+                                    />
+                                </div>
+                            </div>
+                            <div className="card" id="graph">
+                                <h2>Graph</h2>
+                                <div className="form-group">
+                                    <SwitchField name="graph.display" label="Show the forecast graph" />
+                                    <SwitchField name="graph.displayPercip" label="Show the precipitation forecast" disabled={!userStorage.graph.display} />
+                                    <SliderField
+                                        disabled={!userStorage.graph.display}
+                                        name="graph.maxHours"
+                                        label="Hours to show"
+                                        min={2}
+                                        max={24}
+                                        step={2}
                                     />
                                 </div>
                             </div>
@@ -71,30 +124,71 @@ const Options = (props) => {
                                 <div className="form-group">
                                     <FieldArray name="links">
                                         {({ push, remove }) => (
-                                            <div>
+                                            <div style={{ width: '100%' }}>
                                                 {userStorage.links.map((link, index) => (
                                                     <div key={index}>
-                                                        <div className="form-group" style={{ marginBottom: 10 }}>
-                                                            <TextField
-                                                                name={`links.${index}.text`}
-                                                                label="Text"
-                                                                spellCheck={false}
-                                                                width="35"
-                                                            />
-                                                            <TextField
-                                                                name={`links.${index}.link`}
-                                                                label="Link"
-                                                                spellCheck={false}
-                                                                width="52"
-                                                            />
-                                                            <div class="link-remove">
-                                                                <button type="button" onClick={() => remove(index)}><MinusCircle /></button>
+                                                        {link.nested ? (
+                                                            <div>
+                                                                <div className="link-group category">
+                                                                    <TextField
+                                                                        name={`links.${index}.text`}
+                                                                        label="Category name"
+                                                                        fullWidth
+                                                                    />
+                                                                    <div className="link-remove">
+                                                                        <button type="button" onClick={() => remove(index)}><MinusCircle /></button>
+                                                                    </div>
+                                                                </div>
+                                                                <FieldArray name={`links.${index}.nested`}>
+                                                                    {({ push: pushNested, remove: removeNested }) => (
+                                                                        <div>
+                                                                            {link.nested.map((linkNested, indexNested) => (
+                                                                                <div className="link-group nested">
+                                                                                    <TextField
+                                                                                        name={`links.${index}.nested.${indexNested}.text`}
+                                                                                        label="Text"
+                                                                                    />
+                                                                                    <TextField
+                                                                                        name={`links.${index}.nested.${indexNested}.link`}
+                                                                                        label="Link"
+                                                                                        spellCheck={false}
+                                                                                    />
+                                                                                    <div class="link-remove">
+                                                                                        <button type="button" onClick={() => removeNested(indexNested)}><MinusCircle /></button>
+                                                                                    </div>
+                                                                                </div>
+                                                                            ))}
+                                                                            < div className="link-add nested">
+                                                                                <button type="button" onClick={() => pushNested({ text: '', link: '' })}><PlusCircle />&nbsp;&nbsp;Add link</button>
+                                                                            </div>
+                                                                        </div>
+                                                                    )}
+                                                                </FieldArray>
                                                             </div>
-                                                        </div>
+                                                        ) : (
+                                                            <div className="link-group">
+                                                                <TextField
+                                                                    name={`links.${index}.text`}
+                                                                    label="Text"
+                                                                />
+                                                                <TextField
+                                                                    name={`links.${index}.link`}
+                                                                    label="Link"
+                                                                    spellCheck={false}
+                                                                />
+                                                                <div class="link-remove">
+                                                                    <button type="button" onClick={() => remove(index)}><MinusCircle /></button>
+                                                                </div>
+                                                            </div>
+                                                        )}
+
                                                     </div>
                                                 ))}
                                                 <div className="link-add">
                                                     <button type="button" onClick={() => push({ text: '', link: '' })}><PlusCircle />&nbsp;&nbsp;Add link</button>
+                                                </div>
+                                                <div className="link-add">
+                                                    <button type="button" onClick={() => push({ text: '', nested: [] })}><FolderAdd />&nbsp;&nbsp;Add category</button>
                                                 </div>
                                             </div>
                                         )}
@@ -111,31 +205,23 @@ const Options = (props) => {
                                     <ColorField name="colors.c3" label="Tertiary color" disabled={userStorage.colors.autoColor && userStorage.background.autoImage} />
                                 </div>
                             </div>
-                            <div className="card" id="global">
-                                <h2>Global</h2>
-                                <div className="form-group">
-                                    <SwitchField name="global.use24hr" label="Use 24 hour time" />
-                                    <TextField
-                                        name="global.locationOverride"
-                                        label="Location override"
-                                        detail="Override IP-based location estimation"
-                                        placeholder="latitude, longitude"
-                                        spellCheck={false}
-                                    />
-                                    <SliderField
-                                        name="global.uiScale"
-                                        label="User interface size adjust"
-                                        min={0.2}
-                                        max={4}
-                                        step={0.1}
-                                    />
-                                </div>
-                            </div>
+                            <button
+                                className="delete-all card"
+                                onClick={() => {
+                                    if (window.confirm('Are you sure you want to reset all your user options?')) {
+                                        setUserStorage({});
+                                        window.chrome.storage.sync.clear();
+                                        window.location.reload();
+                                    }
+                                }}
+                            >
+                                Reset all settings
+                            </button>
                         </Form>
                     )}
-                </Formik>
-            </div>
-        </div>
+                </Formik >
+            </div >
+        </div >
     )
 }
 
