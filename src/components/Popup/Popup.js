2ec70445f4faeeac220216fe00768dadf2da7be5
rmenon1008 <rmenon1008@gmail.com>
2022-09-03 20:55:48 -0700

    Removes new update card from popup

--- src/components/Popup/Popup.js
+++ src/components/Popup/Popup.js
@@ -1,4 +1,4 @@
-import Logo from "../Logo/Logo"
+import { Logo } from "../Logo/Logo"
 import './Popup.css'
 import { Cog, ChatAlt, Star, ArrowCircleUp, X } from '@styled-icons/heroicons-outline';
 import React, { useContext } from "react";
@@ -15,7 +15,6 @@ const Popup = (props) => {
                 <Logo />
             </div>
             <div className='popup-content'>
-                <NewUpdateCard />
                 <button className='popup-button dark' onClick={options}>
                     Options
                     <Cog />
@@ -31,45 +30,6 @@ const Popup = (props) => {
             </div>
         </div>
     )
-}
-
-const NewUpdateCard = () => {
-    const context = useContext(UserStorageContext);
-    const userStorage = context.userStorage;
-    const setUserStorage = context.setUserStorage;
-
-    const dismiss = () => {
-        setUserStorage((prev) => {
-            return {
-                ...prev,
-                global: {
-                    ...prev.global,
-                    updateCardDismissed: true
-                }
-            }
-        })
-    }
-
-    if (!userStorage.global.updateCardDismissed) {
-        return (
-            <div className="popup-content-card">
-                <span className="heading">Updated to version 2! <ArrowCircleUp /></span>
-
-                <p>Here's what's new:</p>
-                <ul>
-                    <li>More readable links and graphs on images</li>
-                    <li>If you don't like the daily image, you can swap it</li>
-                    <li>Images continue to work when offline</li>
-                    <li>More customization in the options page</li>
-                </ul>
-                <button className='popup-card-close' onClick={dismiss}>
-                    <X />&nbsp;&nbsp;Dismiss
-                </button>
-            </div>
-        )
-    } else {
-        return null
-    }
 }
 
 export default Popup;
