8a96cb37ea4f7ec0d699132348159d043413f22d
rmenon1008 <rmenon1008@gmail.com>
2022-09-03 20:52:30 -0700

    Switches to svg arrow vs font-based

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -6,9 +6,17 @@ const Logo = (props) => {
     return (
         <span className="logo">
             Temperate
-            <span className="logo-green">&#8599;</span>
+            <span className="logo-green"><InlineArrow /></span>
         </span>
     );
 }
 
-export default Logo;
+const InlineArrow = (props) => {
+    return (
+        <svg className="InlineArrow" width="13" height="12" viewBox="0 0 13 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M2.79545 11.8324L0.707387 9.74432L7.15625 3.29545H2.42614L2.44034 0.454545H12.0852V10.0994H9.23011L9.24432 5.38352L2.79545 11.8324Z" fill="#00C853" />
+        </svg>
+    );
+}
+
+export { Logo, InlineArrow };
